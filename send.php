6ae5499a5e0a39a29d245e63eab3fc23d8d22840
Matt Klepac <uibright@gmail.com>
2015-04-04 01:30:06 -0500

    Added better logging

--- send.php
+++ send.php
@@ -1,9 +1,11 @@
 <?php
-if (!isset($_GET['s']) || empty($_GET['s'])) echo "Missing subscription id"; die();
+if (!isset($_GET['s']) || empty($_GET['s'])) { echo "Missing subscription id"; die(); }
 
 $data = array('registration_ids' => array());
 $data['registration_ids'][] = $_GET['s'];
 $data_string = json_encode($data);
+
+echo "data = $data_string\n";
 
 $ch = curl_init('https://android.googleapis.com/gcm/send');
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
@@ -14,7 +16,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'Content-Type: application/json',
     'Content-Length: ' . strlen($data_string))
 );
- 
+
 $result = curl_exec($ch);
-echo("$result\n");
+echo("result = $result\n");
 ?>
